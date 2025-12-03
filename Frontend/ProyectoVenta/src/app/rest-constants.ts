19a5e33bdbc47cc17178542b4d13ca644c3bb8e8
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-12-03 10:08:57 -0600

    hardcode de url

--- Frontend/ProyectoVenta/src/app/rest-constants.ts
+++ Frontend/ProyectoVenta/src/app/rest-constants.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+/*import { Injectable } from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +6,20 @@ import { Injectable } from "@angular/core";
 export class RestConstants {
   // URL relativa - será manejada por nginx
   public readonly API_URL = '/api/';
+
+  public getApiURL(): string {
+    return this.API_URL;
+  }
+}*/
+
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestConstants {
+  // URL HARCODEADA para producción
+  public readonly API_URL = 'https://backend-proyecto-venta-2.onrender.com/api/';
 
   public getApiURL(): string {
     return this.API_URL;
